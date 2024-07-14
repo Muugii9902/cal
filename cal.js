cda1f4a59a606801cc39d879c01a4b65ab6108df
muugii9902 <muugii9902@gmail.com>
2024-07-14 21:44:22 +0800

    calculator

--- cal.js
+++ cal.js
@@ -1,22 +1,64 @@
 // calculator
 const display = document.getElementById("display");
-
 function appendToDisplay(input) {
   display.value += input;
-  let num1 = display.value;
+}
+let num1 = 0;
+let num2 = 0;
+let opbtn = " ";
+function pressOp(op) {
+  num1 = display.value;
+  opbtn = op;
+  display.value = "";
+}
+// function hhm() {
+//   if (opbtn != " ") {
+//     display.value = " ";
+//     // appendToDisplay(input);
+//   }
+// }
+
+// hhm();
+// function calculate() {
+//   num2 = display.value;
+//   display.value = eval(` ${Number(num1)} ${opbtn}  ${Number(num2)}`);
+// }
+
+function calculate() {
+  num2 = display.value;
+  if (opbtn == "+") {
+    display.value = Number(num1) + Number(num2);
+  }
+  if (opbtn == "-") {
+    display.value = Number(num1) - Number(num2);
+  }
+  if (opbtn == "*") {
+    display.value = Number(num1) * Number(num2);
+  }
+  if (opbtn == "/") {
+    display.value = Number(num1) / Number(num2);
+  }
 }
 function clearDisplay() {
   display.value = "";
 }
-function calculate() {
-  try {
-    display.value = eval(display.value);
-  } catch (error) {
-    display.value = "Error";
-  }
-}
 
-function calculate1() {
-  if (button == "+") {
-  }
-}
+// function appendToDisplay(input) {
+//   display.value += input;
+//   let num1 = display.value;
+// }
+// function clearDisplay() {
+//   display.value = "";
+// }
+// function calculate() {
+//   try {
+//     display.value = eval(display.value);
+//   } catch (error) {
+//     display.value = "Error";
+//   }
+// }
+
+// function calculate1() {
+//   if (button == "+") {
+//   }
+// }
